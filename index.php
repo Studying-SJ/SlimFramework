66617bfed3bb38e7a501e1686875ad5f84dd54fa
Samuel-Jesus <samuel.jesan2018@gmail.com>
2022-01-22 03:21:51 -0300

    Adding Injection of dependencies in code controller viewer

--- index.php
+++ index.php
@@ -2,120 +2,124 @@
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
-//Para criar caminhos (rotas) para API, precisamos dizer qual tipo de caminho é (GET, POST, PUT, DELETE...) sendo qua cada um tem suas peculiaridades na forma como 
-// tem que agir.
-// As rotas são definidas os Endpoints (endereços), que vai ou não requerer parâmentros, podendo ser os parâmetros do tipo opcional ou não.
+// ! Para criar caminhos (rotas) para API, precisamos dizer qual tipo de caminho é (GET, POST, PUT, DELETE...) sendo qua cada um tem suas peculiaridades na forma como 
+// ! tem que agir.
+// ! As rotas são definidas os Endpoints (endereços), que vai ou não requerer parâmentros, podendo ser os parâmetros do tipo opcional ou não.
 
 require 'vendor/autoload.php';
 
-$app = new \Slim\App;
 
-$app->get('/retorno', function( Request $request, Response $response){ // utilizando PSR-4 tipando os paramêntros da função anonima.
+//  title: -------------------------- Fazendo Injeção de dependências --------------------------------------
+    $app = new \Slim\App([ // @ Iniciando o objeto Slim para usarmos.
+            'settings' => [ // @ Ativa a exibição de erros na config do objeto.
+                'displayErrorDetails' => true
+            ]
+    ]);
 
-	$response->getBody()->write("Retorno da API"); //Forma correta de emitir um response sem o 'echo'.
-    return $response; //enviando para o front.
+    //title:  Utilizando dependency Injection  com Container Pimple
+    $container = $app->getContainer();// @ Recupera o container do slim que utiliza o pimple.
+    // @ Sua função é guardar classes e funcionalidades que iremos utilizar na aplicação.
+    $container['Home'] = function(){
+        return new MyApp\controllers\Home(new MyApp\View);
+    };
 
-} );
-//Utilizando o verbo POST
-$app->post('/inserir', function( Request $request, Response $response){ // ao utilizar o verbo post é esperado dados enviado com método POST no body da requisição.
-    //recuperando o que foi mandado no body da requisição. 
-    $post = $request->getParseBody();
+    $app->get('/servico', function(Request $request, Response $response){
+        $servico = $this->get('servico');// @  Obtendo funcionalidades que guardamos no container.
+    });
 
-    // code here.
+    // title: Direcionando a rota pra o controller.
+    $app->get('/controller', 'Home:index'); // @ A rota aponta diretamente para o controller, e passa também o parâmetro Req, Res para ele.
 
-    return $response->getBody()->write('Inserido com sucesso!');
+    $app->run();
 
-} );
+// title: ---------------------------------------------------------------------------------------------------
 
-//Utilizando o verbo PUT
-$app->put('/atualizar', function( Request $request, Response $response){ // ao utilizar o verbo post é esperado dados enviado com método POST no body da requisição.
-    //recuperando o que foi mandado no body da requisição. 
-    $id = $request->getAttribute0('id');
+// title: Tipando os parâmetros.
+// $app->get('/retorno', function( Request $request, Response $response){ // title: utilizando PSR-4 tipando os paramêntros da função anônima.
 
-    // code here.
-
-    return $response->getBody()->write('Deletado com sucesso!');
-
-} );
-
-//Utilizando o verbo DELETE
-$app->put('/deletar', function( Request $request, Response $response){ // ao utilizar o verbo post é esperado dados enviado com método POST no body da requisição.
-    //recuperando o que foi mandado no body da requisição. 
-    $post = $request->getParseBody();
-
-    // code here.
-
-    return $response->getBody()->write('Atualizado com sucesso!');
-
-} );
-
-
-
-
-$app->run();
-
-// $app->get('/postagens2', function(){ //Aqui estamos defininindo uma rota do tipo GET e passando seu Endpoint (/postagens2)
-
-// 	echo "Listagem de postagens";
-
+// 	$response->getBody()->write("Retorno da API"); // @ Forma correta de emitir um response sem o 'echo'.
+//     return $response; //enviando para o front.
+// } );
+// title: Utilizando o verbo POST
+// $app->post('/inserir', function( Request $request, Response $response){ // @ ao utilizar o verbo post é esperado dados enviado com método POST no body da requisição.
+//     @ recuperando o que foi mandado no body da requisição. 
+//     $post = $request->getParseBody()
+        // * code here.
+//     return $response->getBody()->write('Inserido com sucesso!');
 // } );
 
-// $app->get('/usuarios[/{id}]', function($request, $response){ //Aqui estamos defininindo uma rota do tipo GET e passando seu Endpoint e um parâmentro não opcional sinalizado pelas '{}' (/usuarios/{parâmetro})
-//                                                             //Tudo que estã dentro de chaves é requerido, assim se não conter não funcionará.
-// 	$id = $request->getAttribute('id'); //Através do request pegamos o parâmetro enviado no Endpoint e a partir daí podemos utiliza-lo.
+// title: Utilizando o verbo PUT
+// $app->put('/atualizar', function( Request $request, Response $response){ // ao utilizar o verbo post é esperado dados enviado com método POST no body da requisição.
+//     @ recuperando o que foi mandado no body da requisição. 
+//     $id = $request->getAttribute0('id');
+        // * code here.
+//     return $response->getBody()->write('Deletado com sucesso!');
+// } );
 
-// 	//Verificar se ID é valido e existe no BD
+// title: Utilizando o verbo DELETE
+// $app->delete('/deletar', function( Request $request, Response $response){ // @ ao utilizar o verbo POST é esperado dados enviado com método POST no body da requisição.
+        // @ recuperando o que foi mandado no body da requisição. 
+//     $post = $request->getParseBody();
+        // * code here.
+//     return $response->getBody()->write('Atualizado com sucesso!');
+// } );
+
+// title: Utilizando o verbo GET
+// $app->get('/postagens2', function(){ // @ Aqui estamos defininindo uma rota do tipo GET e passando seu Endpoint (/postagens2) 
+// 	echo "Listagem de postagens";
+// } );
+
+
+// title: Parâmentros no endpoint
+// title: Parâmetro não OPCIONAL
+// $app->get('/usuarios[/{id}]', function($request, $response){ // @ Aqui estamos defininindo uma rota do tipo GET e passando seu Endpoint e um parâmentro não opcional sinalizado pelas '{}' (/usuarios/{parâmetro})
+    //! Tudo que estã dentro de chaves é requerido, assim se não conter não funcionará.
+// 	$id = $request->getAttribute('id'); // @ Através do request pegamos o parâmetro enviado no Endpoint e a partir daí podemos utiliza-lo.
+
+// 	*Verificar se ID é valido e existe no BD
 
 // 	echo "Listagem de usuarios ou ID: " . $id;
-
 // } );
+// title: Parâmetro OPCIONAL
+// $app->get('/postagens[/{ano}[/{mes}]]', function($request, $response){ // @ Aqui estamos defininindo uma rota do tipo GET e passando seu Endpoint e um parâmentro opcional sinalizado pelas '[]' (/usuarios/{parâmetro})
+    //! Tudo que estã dentro de colchetes é opcional, assim pode ou não conter que funcionará.
 
-// $app->get('/postagens[/{ano}[/{mes}]]', function($request, $response){ //Aqui estamos defininindo uma rota do tipo GET e passando seu Endpoint e um parâmentro opcional sinalizado pelas '[]' (/usuarios/{parâmetro})
-//                                                                        //Tudo que estã dentro de colchetes é opcional, assim pode ou não conter que funcionará.
-	
 // 	$ano = $request->getAttribute('ano');
 // 	$mes = $request->getAttribute('mes');
-
-// 	//Verificar se ID é valido e existe no BD
-
 // 	echo "Listagem de postagens Ano: " . $ano . " mes: " . $mes;
-
 // } );
-
-// $app->get('/lista/{itens:.*}', function($request, $response){ //Podemos tabém tipar dados que é requisitado no parâmetro, colocando o tipo após ':' , sendo assim tornando o tipo um requisito.
+// title: Tipando dados do endpoint
+// $app->get('/lista/{itens:.*}', function($request, $response){ // @ Podemos tabém tipar dados que é requisitado no parâmetro, colocando o tipo após ':' , sendo assim tornando o tipo um requisito.
 	
 // 	$itens = $request->getAttribute('itens');
 
-// 	//Verificar se ID é valido e existe no BD
-// 	//echo $itens;
+// 	echo $itens;
 // 	var_dump(explode("/", $itens));
-
 // } );
 
 
-// /* Nomear rotas */
+// title: Nomeando rotas 
 // $app->get('/blog/postagens/{id}', function($request, $response){ 
 // 	echo "Listar postagem para um ID ";
-// })->setName("blog"); // Podemos definir um nome para a rota, como um alias, afim de utiliza-la posteriormente sem precisar passar toda config.
-
+// })->setName("blog"); // @ Podemos definir um nome para a rota, como um alias, afim de utiliza-la posteriormente sem precisar passar toda config.
+   // title: utilizando rota por seu nome.
 // $app->get('/meusite', function($request, $response){
-	
-// 	$retorno = $this->get("router")->pathFor("blog", ["id" => "10" ] ); //Aqui estamos acessando uma rota que foi setada e passamos os parâmentros requeridos.
-
+// 	$retorno = $this->get("router")->pathFor("blog", ["id" => "10" ] ); // @ Aqui estamos acessando uma rota que foi setada e passamos os parâmentros requeridos.
 // 	echo $retorno;
-
 // });
 
 
-// /* Agrupar rotas */
-// $app->group('/v5', function(){ // O método group permite que agrupemos rotas, afim de utilizar um endpoint padrão para acessar outras rotas do seu contexto, evitando digitar msm nome de parte de rotas em rotas diferentes
-	
+// title: Agrupando rotas 
+// $app->group('/v5', function(){ // @ O método group permite que agrupemos rotas, afim de utilizar um endpoint padrão para acessar outras rotas do seu contexto, evitando digitar msm nome de parte de rotas em rotas diferentes
+
+    // title: Criando rota e exibindo dados com echo.	
 // 	$this->get('/usuarios', function(){
-// 		echo "Listagem de usuarios";
+// 		echo "Listagem de usuarios"; // ! Não é a forma correta de se fazer a exibição na view!
 // 	} );
 
+    // title: Criando rota e exibindo dados com echo.
 // 	$this->get('/postagens', function(){
-// 		echo "Listagem de postagens";
+// 		echo "Listagem de postagens"; // ! Não é a forma correta de se fazer a exibição na view!
 // 	} );
 
 // } );
---
+++ src/MyApp/controllers/Home.php
@@ -0,0 +1,16 @@
+<?php
+namespace MyApp\controllers;
+class Home {
+
+    protected $view;
+    
+    public function __construct($view){
+        $this->view = $view;
+    }
+    public function index( $request, $response){
+        // $view = $this->container->get('View');
+        var_dump($this->view);
+        return $response->write('Testa isso agora!');
+    }
+};
+?>
