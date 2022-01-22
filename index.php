80d1d6763ca6b3574cc6f08f9c5044c2612c5afa
Samuel-Jesus <samuel.jesan2018@gmail.com>
2022-01-22 13:17:30 -0300

    Adding config type response in header response

--- index.php
+++ index.php
@@ -4,33 +4,53 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 
 // ! Para criar caminhos (rotas) para API, precisamos dizer qual tipo de caminho é (GET, POST, PUT, DELETE...) sendo qua cada um tem suas peculiaridades na forma como 
 // ! tem que agir.
-// ! As rotas são definidas os Endpoints (endereços), que vai ou não requerer parâmentros, podendo ser os parâmetros do tipo opcional ou não.
+// ! As rotas são definidas os Endpoints (endereçoFs), que vai ou não requerer parâmentros, podendo ser os parâmetros do tipo opcional ou não.
 
 require 'vendor/autoload.php';
 
+//  title: ---------------------------- Enviando informações no cabeçalho da requisição ----------------------
+$app = new \Slim\App([ // @ Iniciando o objeto Slim para usarmos.
+    'settings' => [ // @ Ativa a exibição de erros na config do objeto.
+        'displayErrorDetails' => true
+    ]
+]);
+// title: Setando configuração do Header 
+    $app->get('/header', function(Request $request, Response $response){
+        $response->withHeader('allow', 'PUT'); // @ Configurando o header para receber apenas requisições do tipo PUT.
 
-//  title: -------------------------- Fazendo Injeção de dependências --------------------------------------
-    $app = new \Slim\App([ // @ Iniciando o objeto Slim para usarmos.
-            'settings' => [ // @ Ativa a exibição de erros na config do objeto.
-                'displayErrorDetails' => true
-            ]
-    ]);
+        $response->withAddedHeader('Content-lengh', 10); // @ Configurnado o header para ter um limite de resposta em caractere.
+    });
+// title: Tipo de respostas
+//@ XML, Json, text
+$app->get('/response_json', function(Request $request, Response $response){
+    return $response->withJson([
+        'nome' => 'Samuel Jesus',
+        'idade' => '19'
+    ]); // @ Transformando resposta em JSON.
+});
+$app->get('/response_xml', function(Request $request, Response $response){
+    $xml = file_get_contents('arquivo');
+    $response->write($xml); // @ Transformando resposta em XML.
+    return $response->withHeader('Content-Type', 'application/xml');
+});
+$app->run();
+// title: ---------------------------------------------------------------------------------------------------
+
+//  title: -------------------------- Fazendo Injeção de dependências com container --------------------------------------
 
     //title:  Utilizando dependency Injection  com Container Pimple
-    $container = $app->getContainer();// @ Recupera o container do slim que utiliza o pimple.
-    // @ Sua função é guardar classes e funcionalidades que iremos utilizar na aplicação.
-    $container['Home'] = function(){
-        return new MyApp\controllers\Home(new MyApp\View);
-    };
+    // $container = $app->getContainer();// @ Recupera o container do slim que utiliza o pimple.
+        // @ Sua função é guardar classes e funcionalidades que iremos utilizar na aplicação.
+    // $container['Home'] = function(){
+    //     return new MyApp\controllers\Home(new MyApp\View);
+    // };
 
-    $app->get('/servico', function(Request $request, Response $response){
-        $servico = $this->get('servico');// @  Obtendo funcionalidades que guardamos no container.
-    });
+    // $app->get('/servico', function(Request $request, Response $response){
+    //     $servico = $this->get('servico');// @  Obtendo funcionalidades que guardamos no container.
+    // });
 
-    // title: Direcionando a rota pra o controller.
-    $app->get('/controller', 'Home:index'); // @ A rota aponta diretamente para o controller, e passa também o parâmetro Req, Res para ele.
-
-    $app->run();
+        // title: Direcionando a rota pra o controller.
+    // $app->get('/controller', 'Home:index'); // @ A rota aponta diretamente para o controller, e passa também o parâmetro Req, Res para ele.
 
 // title: ---------------------------------------------------------------------------------------------------
 
